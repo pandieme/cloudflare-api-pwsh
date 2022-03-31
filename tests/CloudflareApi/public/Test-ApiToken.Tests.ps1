b92c96c43459dcee81383bb8652c59a4de721e1c
Christopher Done <christopher@afewdragons.com>
2022-03-31 22:40:35 +0100

    Fixup test

--- tests/CloudflareApi/public/Test-ApiToken.Tests.ps1
+++ tests/CloudflareApi/public/Test-ApiToken.Tests.ps1
@@ -5,14 +5,13 @@ BeforeAll {
 Describe -Name 'Test-ApiToken' {
     BeforeAll {
         $ApiToken = 'wxnfS4gF3T1AO2YCcO6NShE4o3DT8Ix8dcDYXKVW'
-        $BaseUri = 'https://api.cloudflare.com/client/v4'
     }
 
     It -Name 'Valid token' {
-        Test-ApiToken -BaseUri $BaseUri -ApiToken $ApiToken | Should -Be $true
+        Test-ApiToken -ApiToken $ApiToken | Should -Be $true
     }
 
     It -Name 'Invalid token' {
-        Test-ApiToken -BaseUri $BaseUri -ApiToken 'randomgibberish' | Should -Be $false
+        Test-ApiToken -ApiToken 'randomgibberish' | Should -Be $false
     }
 }
